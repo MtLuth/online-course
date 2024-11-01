3b5635ba1def3f558aa1e441b1f3cb3b8e359ded
Mai Tấn Tài <21110851@student.hcmute.edu.vn>
2024-11-01 10:37:36 +0700

    first commit

---
+++ server/app/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+module.exports = app;
---
+++ server/firebase/firebase.js
@@ -0,0 +1,17 @@
+const { getAnalytics } = require("firebase/analytics");
+const { initializeApp } = require("firebase/app");
+const { getDatabase } = require("firebase/database");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2TbMyOo6htFDd6KrZbyLky3fLl7ZAmC4",
+  authDomain: "online-course-b98be.firebaseapp.com",
+  projectId: "online-course-b98be",
+  storageBucket: "online-course-b98be.appspot.com",
+  messagingSenderId: "948562909891",
+  appId: "1:948562909891:web:f092d8690b829afdd12076",
+  measurementId: "G-BPTSV4QF32",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
