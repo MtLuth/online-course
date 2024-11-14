ae5f6307ba046cf01cc43842435e63bdae482ad3
Mai Tấn Tài <21110851@student.hcmute.edu.vn>
2024-11-14 16:15:55 +0700

    test

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
-dotenv.config({
-  path: "./config.env",
-});
+// import dotenv from "dotenv";
+// dotenv.config({
+//   path: "./config.env",
+// });
 import app from "./app/app.js";
 
 const port = process.env.PORT || 1999;
