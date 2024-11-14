37be3cb52e4d9e65dc2df79c967a9c91a192e266
Mai Tấn Tài <21110851@student.hcmute.edu.vn>
2024-11-14 23:01:35 +0700

    update api create cate

--- server/repository/categoryRepo.js
+++ server/repository/categoryRepo.js
@@ -6,16 +6,27 @@ class Category {
   }
 
   async createCategory(data) {
-    await this.dbRef.doc(data.name).set({ ...data });
+    await this.dbRef.add({ ...data });
     return "Tạo category mới thành công";
   }
 
   async getAllCategory() {
     const snapshot = await this.dbRef.get();
     const categories = snapshot.docs.map((doc) => ({
+      id: doc.id,
       ...doc.data(),
     }));
     return categories;
+  }
+
+  async deleteCategory(name) {
+    await this.dbRef.doc(name).delete();
+    return "Đã xóa category!";
+  }
+
+  async updateCategory(name) {
+    const doc = await this.dbRef.doc(name);
+    // const date = await.
   }
 }
 
