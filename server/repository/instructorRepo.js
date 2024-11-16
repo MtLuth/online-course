31fdb616684014dd24f152d4d823db50fba8191f
Mai Tấn Tài <21110851@student.hcmute.edu.vn>
2024-11-16 20:38:48 +0700

    add update status instructor

---
+++ server/repository/instructorRepo.js
@@ -0,0 +1,29 @@
+import firebaseAdmin from "../firebase/firebaseAdmin";
+
+class InstructorRepo {
+  constructor() {
+    this.dbRef = firebaseAdmin.firestore().collection("instructors");
+  }
+
+  async updateStatus(uid, status) {
+    const doc = await this.dbRef.doc(uid);
+    if (doc) {
+      await doc.update({
+        status: status,
+      });
+    }
+    return "Cập nhật trạng thái thành công!";
+  }
+
+  async getAllInstructor(status) {
+    let query = this.dbRef;
+    if (status != null) {
+      query = query.where("status", "==", status);
+    }
+
+    const snapshot = await query.get();
+    return snapshot;
+  }
+}
+
+export default new InstructorRepo();
